1ff2dfdec90c360db0e729e686657fba9dd15162
Justin Truong <justin.truong@seattlecolleges.edu>
2022-01-26 03:52:02 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-//alert("test");
+//alert("test");    
+
+/*<script>
+    //https://tinyurl.com/dynamic-html-checker
+    document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+      
+    document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
+    
+    //manages mobile nav 
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }   
+ </script>
+ */
